60b88598c388caf9fbefc59e6ecb01ae980587a1
ярик <85576863+randomniht@users.noreply.github.com>
2025-09-29 00:20:47 +0500

    Delete ai_s directory

--- ai_s/project_new/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from core.views import *
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', main),
-    path('promts/<int:promt_id>', promt_detail, name='promt_detail')
-]
